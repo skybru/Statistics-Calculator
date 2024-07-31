e6dd8d93c41b081ba97e33bb455e0c6cad508bdc
skybru <giorgio.bruschi98@gmail.com>
2024-07-31 23:42:13 +0200

    added getMean function

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const calculate = () => {
     const value = document.querySelector("#numbers").value.split();
     const array = value.split(/,\s*/g);
-    const numbers = array.map(item => Number(item));
-    const filtered = numbers.filter(num => {
-        
-    });
-}
+    const numbers = array.map(item => Number(item)).filter(item => !isNaN(item));
+    const mean = getMean(numbers);
+    document.querySelector("#mean").textContent = mean;
+};
+
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+    const sorted = array.sort((a, b) => a - b);
+    const isEven = array.length % 2 === 0;
+    const oddListMedian = array[Math.floor(array.length / 2)];
+    const evenMiddles = () => [array[array.length / 2], array[(array.length / 2) - 1]];
+    const evenListMedian = getMean(evenMiddles);
+};
