d0cada1d96eff5b8e67490a70dc610d993272bb1
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-07-31 17:44:14 +0200

    creating calculate function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const calculate = () => {
-    
+    const value = document.querySelector("#numbers").value.split();
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(item => Number(item));
+    const filtered = numbers.filter(num => {
+        
+    });
 }
