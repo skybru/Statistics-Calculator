3b52b9b8e97bc0bcc9729210a9314969af2645fe
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-08-01 12:09:30 +0200

    added median function

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const calculate = () => {
     const numbers = array.map(item => Number(item)).filter(item => !isNaN(item));
     const mean = getMean(numbers);
     document.querySelector("#mean").textContent = mean;
+
+    const median = getMedian(numbers);
+    document.querySelector("#median").textContent = median;
+
+
 };
 
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
     const sorted = array.sort((a, b) => a - b);
-    const isEven = array.length % 2 === 0;
-    const oddListMedian = array[Math.floor(array.length / 2)];
-    const evenMiddles = () => [array[array.length / 2], array[(array.length / 2) - 1]];
-    const evenListMedian = getMean(evenMiddles);
+    if (sorted.length % 2 === 0) {
+      return getMean([sorted[(sorted.length / 2) - 1], sorted[sorted.length / 2]]);
+    } else {
+      return sorted[Math.floor(sorted.length / 2)];
+    }
 };
+
